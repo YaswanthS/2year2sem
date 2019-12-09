4cf41d29ab213af60dcb30c0b6ee46acf2f3f683
YaswanthS <52109258+YaswanthS@users.noreply.github.com>
2019-12-09 20:24:06 +0530

    Create multiclass.java

---
+++ multiclass.java
@@ -0,0 +1,19 @@
+class 
+{
+ int a,b;
+ void getdata(int m,int n)
+ {
+  a=m;
+  b=n;
+  }
+  class B
+  {
+  public static void main(String args[])
+  {
+   int c;
+   A obj=new obj();
+   obj.getdata(10,20);
+   c=obj.a+abj.b;
+   System.out.println("sum +",+c);
+   }
+   }
